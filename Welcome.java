2be4b7313eee1e694fc65fdf9b4f904408e07481
jmmenah <jmmenah@gmail.com>
2019-09-25 19:47:19 +0200

    new

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,5 @@
 public class Welcome {
    public static void main(String[] args) {
-     System.out.println("Welcome to Java 2019!");
+     System.out.println("NEW Welcome to Java 2019!");
    }
 }
